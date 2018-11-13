313a8d0e56501538b52a852d7bc85c32b2edc9a2
Isaac Avram <avrisaac555@gmail.com>
2018-11-13 13:47:16 -0800

    Things

---
+++ app/Youtube.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Youtube where
+
+import           Control.Monad.IO.Class
+-- "qualified" imports into a namespace
+import qualified Data.Aeson                 as JSON
+import qualified Data.ByteString.Lazy.Char8 as L8
+import           Network.HTTP.Simple
+
+-- Takes an optional Int, a video id and an API key to return a URL for the Search.list API call
+buildListURL :: Maybe Int -> String -> String -> String
+buildListURL Nothing vID key = "https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=" ++ vID ++ "&type=video&key=" ++ key
+buildListURL (Just maxCount) vID key = "https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=" ++ vID ++ "&maxResults=" ++ show maxCount ++ "&type=video&key=" ++ key
+
+-- Performs a request, and assumes the response will be JSON
+performJSONRequest :: (MonadIO io) => Request -> io JSON.Value
+performJSONRequest req = getResponseBody <$> httpJSON req
