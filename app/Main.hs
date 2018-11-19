77a39a0634afa964b2429b1a96ad9f39953018ef
Isaac Avram <avrisaac555@gmail.com>
2018-11-19 15:27:44 -0800

    Oopsed a syntax

--- app/Main.hs
+++ app/Main.hs
@@ -12,7 +12,7 @@ type VideoTree = Tree VideoSnippet
 
 main :: IO ()
 main = do
-    (flags, (beginId, apiKey) <- catch (getArgs >>= parseArgs) $ \e -> do
+    (flags, (beginId, apiKey)) <- catch (getArgs >>= parseArgs) $ \e -> do
                         let err = show (e :: IOException)
                         putStrLn err
                         exitWith $ ExitFailure 1
