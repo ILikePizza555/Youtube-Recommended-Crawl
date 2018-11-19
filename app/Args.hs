e038e93b2f307cd1ebbf6781ac59c82e4a9eaa5e
Isaac Avram <avrisaac555@gmail.com>
2018-11-18 16:49:51 -0800

    Fixed some build issues

--- app/Args.hs
+++ app/Args.hs
@@ -9,15 +9,18 @@ data Flag = Verbose | Tags [String] | Output String | Help deriving (Show)
 convArgTags :: String -> Flag
 convArgTags s = Tags $ splitOn "," s
 
-flags = [
-    Option ["v"] ["verbose"] NoArg Verbose                  "Verbose output."
-    Option ["t"] ["tags"]    (ReqArg convArgTags "TAGS")    "A comma-separated list of tags to look for."
-    Option ["o"] ["output"]  (OptArg Output "FILE")         "Outputs results to FILE."
-    Option []    ["help"]    NoArg Help                     "Prints help output and exits."
-]
+convArgOutput :: Maybe String -> Flag
+convArgOutput (Just s) = Output s
+convArgOutput Nothing = Output ""
+
+flags :: [OptDescr Flag]
+flags = [ Option ['v'] ["verbose"] (NoArg Verbose)                "Verbose output."
+        , Option ['t'] ["tags"]    (ReqArg convArgTags "TAGS")    "A comma-separated list of tags to look for."
+        , Option ['o'] ["output"]  (OptArg convArgOutput "FILE")  "Outputs results to FILE."
+        , Option []    ["help"]    (NoArg Help)                   "Prints help output and exits." ]
 
 -- Parses the arguments given the command-line
 parseArgs :: [String] ->  IO ([Flag], [String])
 parseArgs = case getOpt RequireOrder flags of
                 (fl, sl, _) -> return (fl, sl)
-                (_, _, err) -> ioError userError $ concat err ++ usageInfo "Usage: yrc [--help] [-v] [-o FILE] <-t tag1,tag2,tag3,...> BEGIN_ID" flags
+                (_, _, err) -> ioError . userError $ concat err ++ usageInfo "Usage: yrc [--help] [-v] [-o FILE] <-t tag1,tag2,tag3,...> BEGIN_ID" flags
