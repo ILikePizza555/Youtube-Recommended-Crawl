2a95a8c3c7ca3a28f5a1b99a4e8fc14181335f73
Isaac Avram <avrisaac555@gmail.com>
2018-11-18 19:57:13 -0800

    seperated where part into usageHeader and usageStr

--- app/Args.hs
+++ app/Args.hs
@@ -16,12 +16,17 @@ convArgOutput Nothing = Output ""
 flags :: [OptDescr Flag]
 flags = [ Option ['v'] ["verbose"] (NoArg Verbose)                "Verbose output."
         , Option ['t'] ["tags"]    (ReqArg convArgTags "TAGS")    "A comma-separated list of tags to look for."
-        , Option ['o'] ["output"]  (OptArg convArgOutput "FILE")  "Outputs results to FILE."
+        , Option ['o'] ["output"]  (ReqArg convArgOutput "FILE")  "Outputs results to FILE."
         , Option []    ["help"]    (NoArg Help)                   "Prints help output and exits." ]
+
+usageHeader :: String
+usageHeader = "Usage: yrc [--help] [-v] [-o FILE] <-t tag1,tag2,tag3,...> BEGIN_ID"
+
+usageStr :: String
+usageStr = usageInfo usageHeader flags
 
 -- Parses the arguments given the command-line
 parseArgs :: [String] ->  IO ([Flag], [String])
 parseArgs xs = case getOpt RequireOrder flags xs of
                 (fl, sl, []) -> return (fl, sl)
-                (_, _, errl) -> ioError . userError $ concat errl ++ usageInfo header flags
-                where header = "Usage: yrc [--help] [-v] [-o FILE] <-t tag1,tag2,tag3,...> BEGIN_ID"
+                (_, _, errl) -> ioError . userError $ concat errl ++ usageStr
