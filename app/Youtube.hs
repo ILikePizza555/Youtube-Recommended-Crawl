35caceb92b541a11589a92f65d34c0bbf201eced
Isaac Avram <avrisaac555@gmail.com>
2018-11-20 14:13:51 -0800

    Added parseVideoIdList and parseVideoIdObj

--- app/Youtube.hs
+++ app/Youtube.hs
@@ -11,6 +11,7 @@ import qualified Data.ByteString.Lazy.Char8 as L8
 import           Data.Maybe
 import           Data.Time
 import           Data.HashMap.Strict        as HM
+import qualified Data.Vector                as V
 import           Network.HTTP.Simple
 
 data VideoSnippet = VideoSnippet {
@@ -24,13 +25,9 @@ data VideoSnippet = VideoSnippet {
     } deriving Show
 
 instance JSON.FromJSON VideoSnippet where
-    -- parseJSON :: Value -> Parser a
-    -- withObject :: String -> (Object -> Parser a) -> Value -> Parser a
+    -- Parses object found in "items" array of video.list 
     parseJSON = withObject "VideoSnippet" $ \obj -> do
-        id_ <- case HM.lookup "id" obj of
-            Just (String sv) -> return sv
-            Just v -> (withObject "idObj" $ \o -> o .: "videoId") v
-            Nothing -> fail "No field 'id'"
+        id_     <- obj .: "id"
         
         sObj    <- obj .: "snippet"
 
@@ -40,22 +37,23 @@ instance JSON.FromJSON VideoSnippet where
         cID_    <- sObj .: "channelId"
         cName_  <- sObj .: "channelTitle"
         tags_   <- sObj .:? "tags" .!= []
-        return $ VideoSnippet (show id_) pub_ title_ desc_ cID_ cName_ tags_
+        return $ VideoSnippet id_ pub_ title_ desc_ cID_ cName_ tags_
 
--- Parses a JSON array with key "items" to a list of VideoSnippet
-parseItems :: JSON.Value -> [VideoSnippet]
--- parse :: (a -> Parser b) -> a -> Result b
--- Need to use withObject to convert `v` into on Object value to use with `obj`
-parseItems v = let parser = withObject "items" $ \obj -> obj .: "items" in 
-    case parse parser v of
-        Error _ -> []
-        Success a -> a
+-- Parses the videoId object into a string
+-- E.g { "id": {"videoID": "desiredString" } }
+parseVideoIdObj :: JSON.Value -> Parser Text
+parseVideoIdObj = withObject "wrapper object" $ \obj -> do
+    idObj <- obj .: "id"
+    return $ idObj .: "videoId"
 
+-- Parses a list of videoId objects
+parseVideoIdList :: JSON.Value -> Parser [Text]
+parseVideoIdList = withArray "array" $ \arr -> mapM parseVideoIdObj (V.toList arr)
 
 -- Takes an optional Int, a video id and an API key to return a URL for the Search.list API call
 buildSearchListURL :: Maybe Int -> String -> String -> String
-buildSearchListURL Nothing vID key = "https://www.googleapis.com/youtube/v3/search?part=snippet&fields=items(id%2Csnippet(channelId%2CchannelTitle%2Cdescription%2CpublishedAt%2Ctitle))&relatedToVideoId=" ++ vID ++ "&type=video&key=" ++ key
-buildSearchListURL (Just maxCount) vID key = "https://www.googleapis.com/youtube/v3/search?part=snippet&fields=items(id%2Csnippet(channelId%2CchannelTitle%2Cdescription%2CpublishedAt%2Ctitle))&relatedToVideoId=" ++ vID ++ "&maxResults=" ++ show maxCount ++ "&type=video&key=" ++ key
+buildSearchListURL Nothing vID key = "https://www.googleapis.com/youtube/v3/search?part=snippet&fields=items%2Fid%2FvideoId&relatedToVideoId=" ++ vID ++ "&type=video&key=" ++ key
+buildSearchListURL (Just maxCount) vID key = "https://www.googleapis.com/youtube/v3/search?part=snippet&fields=items%2Fid%2FvideoId&relatedToVideoId=" ++ vID ++ "&maxResults=" ++ show maxCount ++ "&type=video&key=" ++ key
 
 -- Takes a video id and an api key to return a URL for the Video.list API call
 buildVideoListURL :: String -> String -> String
