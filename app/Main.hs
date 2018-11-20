b5a8c9ea56abde2b080bea31578c92573986fc09
Isaac Avram <avrisaac555@gmail.com>
2018-11-20 15:54:56 -0800

    added treeGrowth function

--- app/Main.hs
+++ app/Main.hs
@@ -4,11 +4,30 @@ import Args (parseArgs)
 import Youtube
 
 import Control.Exception
+import Control.Monad
 import Data.Tree
 import System.Environment (getArgs)
 import System.Exit
 
 type VideoTree = Tree VideoSnippet
+
+data DepthParam = DepthParam {videoID :: String, depth :: Int} deriving (Show, Eq) 
+
+treeGrowth :: Bool -> String -> Int -> DepthParam -> IO (VideoSnippet, [DepthParam])
+treeGrowth verbose apiKey maxDepth dp = do
+    let vid = videoID dp
+    let dep = depth dp
+
+    when verbose $ putStrLn "[Verbose] Performing video.list request on " ++ vid
+    vidSnippet <- buildVideoListRequest vid apiKey >>= performJSONRequest
+
+    if dep == maxDept then do
+        when verbose $ putStrLn "[Verbose] Stopping growth at max depth."
+        return (vidSnippet, [])
+    else do
+        when verbose $ putStrLn "[Verbose] Performing search.list request on " ++ vid
+        
+
 
 main :: IO ()
 main = do
