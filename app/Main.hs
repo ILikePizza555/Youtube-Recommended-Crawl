efa4c75bbbe9a3e30d273a8511917ca78b25a3e0
Isaac Avram <avrisaac555@gmail.com>
2018-11-21 16:57:38 -0800

    Error checking in main

--- app/Main.hs
+++ app/Main.hs
@@ -8,19 +8,30 @@ import Control.Monad
 import Data.Tree
 import System.Environment (getArgs)
 import System.Exit
+import System.IO
 
 type VideoTree = Tree VideoSnippet
 
 data DepthParam = DepthParam {videoID :: String, depth :: Int} deriving (Show, Eq) 
+
+-- Prints an error message to stderr and exits the program
+errExitMessage :: String -> IO ()
+errExitMessage s = do
+    hPutStrLn stderr s
+    exitWith $ ExitFailure 1
 
 treeGrowth :: Bool -> String -> Int -> DepthParam -> IO (VideoSnippet, [DepthParam])
 treeGrowth verbose apiKey maxDepth dp = do
     let vid = videoID dp
     let dep = depth dp
 
+    -- Perform the video.list api request and parse the response. Quit the program on parse failure or no response.
     when verbose (putStrLn $ "[Verbose] Performing video.list request on " ++ vid)
-    vidSnippet <- head . parseVideoListResponse <$> (buildVideoListRequest vid apiKey >>= performJSONRequest)
+    parseResult <- case parseVideoListResponse <$> (buildVideoListRequest vid apiKey >>= performJSONRequest) of
+                        Left errStr -> errExitMessage errStr
+                        Right res -> if null res then errExitMessage "Recieved empty response from Youtube." else head res
 
+    -- Check if we've reached maxDepth, if we have, return no seed values.
     if dep == maxDepth then do
         when verbose (putStrLn "[Verbose] Stopping growth at max depth.")
         return (vidSnippet, [])
@@ -35,8 +46,7 @@ main :: IO ()
 main = do
     (flags, (beginId, apiKey)) <- catch (getArgs >>= parseArgs) $ \e -> do
                         let err = show (e :: IOException)
-                        putStrLn err
-                        exitWith $ ExitFailure 1
+                        errExitMessage err
     
     putStrLn $ "Starting with " ++ beginId
     exitWith ExitSuccess
